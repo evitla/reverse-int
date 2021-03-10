f69c615bd17c44f2366ea7ccb72d3ab3fa96a665
Dinmukhamed <s.dinmuhamed@gmail.com>
2021-03-10 16:30:10 +0600

    fix: fix reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  n = Math.abs(n);
+  const nums = [...n.toString()];
   
+  for (let i = 0; i < nums.length / 2; i++) {
+    const idx = nums.length - 1 - i;
+    [nums[i], nums[idx]] = [nums[idx], nums[i]];
+  }
+  
+  return Number.parseInt(nums.join(""));
 }
